99331934430b313a875f233da89584ca4d4e31c8
Vitor Teixeira <vitoroliv@outlook.com>
2021-11-11 09:37:49 -0300

    Adicionando os botões 'Saiu'  e  'Entrou' , com funções basicas ao pressionar.

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,11 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 //-- Iniciando App --//
-void main () {
+void main() {
   runApp(MyApp());
 }
 //-! Iniciando App --//
-
 
 //-- Abrinddo Classe STFULL --//
 
@@ -20,36 +18,71 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+//-- Função Decrementar --//
+void decrement (){
+  print("decrementar");
+}
+//-! Função Decrementar --//
+
+//-- Função incrementar --//
+void increment (){
+  print("incrementar");
+}
+
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.red, //Cor do fundo da Tela
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center, //Centralizando widgets
-          children: [
-            const Text (
-              "Pode Entrar",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.w700,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.red, //Cor do fundo da Tela
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center, //Centralizando widgets
+            children: [
+              const Text(
+                "Pode Entrar",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
-            ),
-            const Text(
-              "0",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 100,
-                fontWeight: FontWeight.w700,
+              const Text(
+                "0",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 100,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
-            )
-          ],
-        ),
-      )
-    );
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: decrement,
+                    child: Text(
+                      "Saiu",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: increment,
+                    child: Text(
+                      "Entrou",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
 //-! Abrinddo Classe STFULL --//
